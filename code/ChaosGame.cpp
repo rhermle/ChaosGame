0d5f472bdc12422a99f64bfd8f10f01399789555
admin <admin@BCS-Ubuntu-07.lrccd.ad.losrios.edu>
2022-09-14 11:07:37 -0700

    Starter Code

--- code/ChaosGame.cpp
+++ code/ChaosGame.cpp
@@ -1,20 +1,90 @@
-// ChaosGame.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+// Include important C++ libraries here
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <iostream>
+#include <sstream>
+#include <vector>
+
+// Make code easier to type with "using namespace"
+using namespace sf;
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Create a video mode object
+	VideoMode vm(1920, 1080);
+	// Create and open a window for the game
+	RenderWindow window(vm, "Timber Game!!", Style::Default);
+
+    vector<Vector2f> vertices;
+    vector<Vector2f> points;
+
+	while (window.isOpen())
+	{
+        /*
+		****************************************
+		Handle the players input
+		****************************************
+		*/
+        Event event;
+		while (window.pollEvent(event))
+		{
+            if (event.type == Event::Closed)
+            {
+				// Quit the game when the window is closed
+				window.close();
+            }
+            if (event.type == sf::Event::MouseButtonPressed)
+            {
+                if (event.mouseButton.button == sf::Mouse::Left)
+                {
+                    std::cout << "the left button was pressed" << std::endl;
+                    std::cout << "mouse x: " << event.mouseButton.x << std::endl;
+                    std::cout << "mouse y: " << event.mouseButton.y << std::endl;
+
+                    if(vertices.size() < 3)
+                    {
+                        vertices.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
+                    }
+                    else if(points.size() == 0)
+                    {
+                        ///fourth click
+                        ///push back to points vector
+                    }
+                }
+            }
+        }
+        if (Keyboard::isKeyPressed(Keyboard::Escape))
+		{
+			window.close();
+		}
+        /*
+		****************************************
+		Update
+		****************************************
+		*/
+
+        if(points.size() > 0)
+        {
+            ///generate more point(s)
+            ///select random vertex
+            ///calculate midpoint between random vertex and the last point in the vector
+            ///push back the newly generated coord.
+        }
+
+        /*
+		****************************************
+		Draw
+		****************************************
+		*/
+        window.clear();
+        for(int i = 0; i < vertices.size(); i++)
+        {
+            RectangleShape rect(Vector2f(10,10));
+            rect.setPosition(Vector2f(vertices[i].x, vertices[i].y));
+            rect.setFillColor(Color::Blue);
+            window.draw(rect);
+        }
+        window.display();
+    }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
