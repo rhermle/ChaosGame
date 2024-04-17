741d0f001b50cf4d4f28b6d97956f32736e6c111
rhermle <rhermle@gmail.com>
2024-04-17 05:15:34 -0700

    Update ChaosGame.cpp

--- code/ChaosGame.cpp
+++ code/ChaosGame.cpp
@@ -11,80 +11,86 @@ using namespace std;
 
 int main()
 {
-    // Create a video mode object
+	// Create a video mode object
 	VideoMode vm(1920, 1080);
 	// Create and open a window for the game
 	RenderWindow window(vm, "Timber Game!!", Style::Default);
 
-    vector<Vector2f> vertices;
-    vector<Vector2f> points;
+	//resize window for different resolutions
+	View view;
+	view.setSize(VideoMode::getDesktopMode().width, VideoMode::getDesktopMode().height);
+	view.setCenter(VideoMode::getDesktopMode().width / 2, VideoMode::getDesktopMode().height / 2);
+	window.setView(view);
+	
+	vector<Vector2f> vertices;
+	vector<Vector2f> points;
 
 	while (window.isOpen())
 	{
-        /*
+		/*
 		****************************************
 		Handle the players input
 		****************************************
 		*/
-        Event event;
+		Event event;
 		while (window.pollEvent(event))
 		{
-            if (event.type == Event::Closed)
-            {
-				// Quit the game when the window is closed
-				window.close();
-            }
-            if (event.type == sf::Event::MouseButtonPressed)
-            {
-                if (event.mouseButton.button == sf::Mouse::Left)
-                {
-                    std::cout << "the left button was pressed" << std::endl;
-                    std::cout << "mouse x: " << event.mouseButton.x << std::endl;
-                    std::cout << "mouse y: " << event.mouseButton.y << std::endl;
-
-                    if(vertices.size() < 3)
-                    {
-                        vertices.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
-                    }
-                    else if(points.size() == 0)
-                    {
-                        ///fourth click
-                        ///push back to points vector
-                    }
-                }
-            }
-        }
-        if (Keyboard::isKeyPressed(Keyboard::Escape))
+		    if (event.type == Event::Closed)
+		    {
+					// Quit the game when the window is closed
+					window.close();
+		    }
+		    if (event.type == sf::Event::MouseButtonPressed)
+		    {
+			if (event.mouseButton.button == sf::Mouse::Left)
+			{
+			    std::cout << "the left button was pressed" << std::endl;
+			    std::cout << "mouse x: " << event.mouseButton.x << std::endl;
+			    std::cout << "mouse y: " << event.mouseButton.y << std::endl;
+	
+			    if(vertices.size() < 3)
+			    {
+				vertices.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
+			    }
+			    else if(points.size() == 0)
+			    {
+				///fourth click
+				///push back to points vector
+			    }
+			}
+		    }
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Escape))
 		{
 			window.close();
 		}
-        /*
+		/*
 		****************************************
 		Update
 		****************************************
 		*/
-
-        if(points.size() > 0)
-        {
-            ///generate more point(s)
-            ///select random vertex
-            ///calculate midpoint between random vertex and the last point in the vector
-            ///push back the newly generated coord.
-        }
-
-        /*
+	
+		if(points.size() > 0)
+		{
+		    ///generate more point(s)
+		    ///select random vertex
+		    ///calculate midpoint between random vertex and the last point in the vector
+		    ///push back the newly generated coord.
+		}
+	
+		/*
 		****************************************
 		Draw
 		****************************************
 		*/
-        window.clear();
-        for(int i = 0; i < vertices.size(); i++)
-        {
-            RectangleShape rect(Vector2f(10,10));
-            rect.setPosition(Vector2f(vertices[i].x, vertices[i].y));
-            rect.setFillColor(Color::Blue);
-            window.draw(rect);
-        }
-        window.display();
-    }
+		window.clear();
+		for(int i = 0; i < vertices.size(); i++)
+		{
+		    RectangleShape rect(Vector2f(10,10));
+		    rect.setPosition(Vector2f(vertices[i].x, vertices[i].y));
+		    rect.setFillColor(Color::Blue);
+		    window.draw(rect);
+		}
+		window.display();
+	}
 }
